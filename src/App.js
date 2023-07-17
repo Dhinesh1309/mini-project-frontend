6c22306cd5c1c6de67cc78d603be9e44f035f88b
Vignesh <dhinesh139ad@gmail.com>
2023-07-17 19:28:57 +0530

    sidebar with out minimizing icon. sidebar working.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,74 @@
-import RouterIndex from "./routes/RouterIndex";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Employees,
+  Customers,
+  Kanban,
+  Editor,
+} from "./pages";
+import "./App.css";
+import { useStateContext } from "./contexts/ContextProvider";
 
 function App() {
-  return <h1>Hello</h1>;
+  const { activeMenu } = useStateContext();
+
+  return (
+    <>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              style={{ background: "blue", borderRadius: "50%" }}
+              className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray">
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+        {activeMenu ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar />
+          </div>
+        ) : (
+          <div
+            className=" sidebar fixed dark:bg-secondary-dark-bg bg-white"
+            style={{ width: "5%" }}>
+            <Sidebar />
+          </div>
+        )}
+        <div
+          className={
+            activeMenu
+              ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+              : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-5 "
+          }>
+          <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+            <Navbar />
+          </div>
+        </div>
+        <Routes>
+          {/* dashboard  */}
+          <Route path="/" element={<Ecommerce />} />
+          <Route path="/ecommerce" element={<Ecommerce />} />
+
+          {/* pages  */}
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/customers" element={<Customers />} />
+
+          {/* apps  */}
+          <Route path="/kanban" element={<Kanban />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
